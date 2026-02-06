ff7ec5f930fffc83f1cf69170ed70a2b91856150
Yashtomar001 <tomaryash2007@gmail.com>
2026-02-06 11:34:29 +0530

    Updated feature

---
+++ script6.js
@@ -0,0 +1,25 @@
+// const para = document.getElementsByTagName("p");
+//     for (let i = 0; i < para.length; i++) {
+//     para[i].style.color = "green";  
+//     para[i].style.fontSize = "35px";    
+//     para[i].style.fontWeight = "bold";
+//     }
+
+
+// const para = document.getElementsByTagName("p");
+// Array.from(para).forEach(element => {
+//     if(index%2===0){
+//         element.style.color = "green";
+//     }
+//     else{
+//         element.style.color = "red";    
+//     }
+//     element.style.fontSize = "35px";
+//     element.style.fontWeight = "bold";
+// });
+
+// create a new element and append
+// const newPara = document.createElement("p");
+// newPara.textContent = "This is a new paragraph created using JavaScript.";
+// console.log(newPara);   
+// document.getElementById("content").append(newPara);
